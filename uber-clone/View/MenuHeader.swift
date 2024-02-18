c2e3108f522e8afa5ccfb23e0bcf99073e4bc168
Girish <ramloul24@gmail.com>
2024-02-17 23:13:09 -0500

    All code

---
+++ uber-clone/View/MenuHeader.swift
@@ -0,0 +1,79 @@
+//
+//  MenuHeader.swift
+//  uber-clone
+//
+//  Created by Ted Hyeong on 27/10/2020.
+//
+
+import UIKit
+
+class MenuHeader: UIView {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    
+    private lazy var profileImageView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        
+        view.addSubview(initialLabel)
+        initialLabel.centerX(inView: view)
+        initialLabel.centerY(inView: view)
+        return view
+    }()
+    
+    private lazy var initialLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 42)
+        label.textColor = .white
+        label.text = user.firstInitial
+        return label
+    }()
+    
+    private lazy var fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .white
+        label.text = user.fullname
+        return label
+    }()
+    
+    private lazy var  emailLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = user.email
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init(user: User, frame: CGRect) {
+        self.user = user
+        super.init(frame: frame)
+        
+        backgroundColor = .backgroundColor
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 4, paddingLeft: 12,
+                                width: 64, height: 64)
+        profileImageView.layer.cornerRadius = 64 / 2
+        
+        let stack = UIStackView(arrangedSubviews: [fullnameLabel, emailLabel])
+        stack.distribution = .fillEqually
+        stack.spacing = 4
+        stack.axis = .vertical
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView,
+                      leftAnchor: profileImageView.rightAnchor,
+                      paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+}
